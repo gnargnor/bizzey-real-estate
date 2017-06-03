e4801369a0be703ced02c11ccb90963f97564203
Logan Kelly <logan.r.kelly@gmail.com>
2017-06-03 16:07:08 -0500

    added readme

--- server/app.js
+++ server/app.js
@@ -7,31 +7,32 @@ var bodyParser = require('body-parser');
 
 var mongoose = require("mongoose");
 var mongoURI = "mongodb://moo:worm@ds149800.mlab.com:49800/listings";
+// var mongoURI = "mongodb://localhost:27017" - for use on local machines
 var MongoDB = mongoose.connect(mongoURI).connection;
 
 //If there is an error connecting to the database, let us know!
-MongoDB.on("error", function(err){
-  console.log("Mongo Connection Error :" + err);
+MongoDB.on("error", function(err) {
+    console.log("Mongo Connection Error :" + err);
 });
 
 //If we successfully hooked up to the database, let us know!
-MongoDB.once("open", function(){
-  console.log("Mongo");
+MongoDB.once("open", function() {
+    console.log("Mongo");
 });
 
 app.set('port', port);
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
 app.use('/realestate', realestate);
 
 
 
-app.get('/', function(){
-  res.send(index);
-} );
+app.get('/', function() {
+    res.send(index);
+});
 
-app.listen(port, function (req, res){
-  console.log('listening on port ', port);
+app.listen(port, function(req, res) {
+    console.log('listening on port ', port);
 });
